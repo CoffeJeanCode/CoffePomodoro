2393ce02b3e59a2af682433fd60cba01d07ee0a7
Jean Pierre Ortiz Murcia <ortizmurciajeanpierre@gmail.com>
2022-09-11 14:48:40 -0500

    hotfix: delete button of chakra

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -1,12 +1,7 @@
-import { Button, Icon } from "@chakra-ui/react";
 import { FaChartBar } from "react-icons/fa";
 
 const Stats = () => {
-  return (
-    <>
-      <Button leftIcon={<Icon as={FaChartBar} />}>Stats</Button>
-    </>
-  );
+  return <>Stats</>;
 };
 
 export default Stats;
