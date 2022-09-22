e9e7394e378c18cc879b75e01f58a69c0e6cbd2f
Jean Pierre Ortiz Murcia <ortizmurciajeanpierre@gmail.com>
2022-09-22 07:55:18 -0500

    hotfix: changing am and pm condition

--- src/utils/time.util.ts
+++ src/utils/time.util.ts
@@ -6,7 +6,7 @@ export const secondsToMilliseconds = multiply(1000);
 export const millisecondsToSeconds = compose(Math.round, divide(__, 1000));
 export const getSeconds = modulo(__, 60);
 export const formatTime = (n: number) => (n < 10 ? `0${n}` : String(n));
-export const formatSun = (n: number) => (n < 12 ? "p.m" : "a.m");
+export const formatSun = (n: number) => (n > 12 ? "p.m" : "a.m");
 
 export const getTime = (rawSeconds: number) => {
   const minutes = secondsToMinutes(rawSeconds);
@@ -19,8 +19,8 @@ export const getTime = (rawSeconds: number) => {
 export const getEndTime = (timestamp: number) => {
   const end = new Date(timestamp);
   const hour = end.getHours();
-  const adjuestedHour = hour > 12 ? hour - 12 : hour;
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
   const minutes = formatTime(end.getMinutes());
 
-  return `${adjuestedHour}:${minutes} ${formatSun(adjuestedHour)}`;
+  return `${adjustedHour}:${minutes} ${formatSun(adjustedHour)}`;
 };
