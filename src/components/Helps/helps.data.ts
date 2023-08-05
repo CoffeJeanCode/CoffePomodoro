a481bcfe0c3f844f2e502f225c5f548a5a852a3e
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2023-08-05 17:15:34 +0000

    feat: add full screen helper

--- src/components/Helps/helps.data.ts
+++ src/components/Helps/helps.data.ts
@@ -1,10 +1,19 @@
 export const helps = {
-	breaks: ["Short Nap", "Clean Up", "Meditate", "Posture", "Go outside", "Stretches", "Mini Exersise"],
-	hotkeys: {
-		main: [
-			{ key: "Space", action: "Toggle timer" },
-			{ key: "N", action: "Next timer" },
-			{ key: "S", action: "Stop timer" },
-		],
-	},
+  breaks: [
+    "Short Nap",
+    "Clean Up",
+    "Meditate",
+    "Posture",
+    "Go outside",
+    "Stretches",
+    "Mini Exersise",
+  ],
+  hotkeys: {
+    main: [
+      { key: "Space", action: "Toggle timer" },
+      { key: "N", action: "Next timer" },
+      { key: "S", action: "Stop timer" },
+      { key: "F", action: "Full Screen" },
+    ],
+  },
 };
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -11,72 +11,86 @@ import TimerText from "./TimerText";
 import useTimer from "./useTimer";
 
 const Timer = () => {
-	const { handleNextTimer, handleStopTimer, handleToggleTimer, isRunning, remainingTimeText } = useTimer();
-	const { mode, favIcon, setFavIcon } = useInfoState();
-	const [isFullScreen, setIsFullScreen] = useState(false);
+  const {
+    handleNextTimer,
+    handleStopTimer,
+    handleToggleTimer,
+    isRunning,
+    remainingTimeText,
+  } = useTimer();
+  const { mode, favIcon, setFavIcon } = useInfoState();
+  const [isFullScreen, setIsFullScreen] = useState(false);
 
-	useEffect(() => {
-		setFavIcon(mode === Mode.Pomodoro ? FavIcon.work : FavIcon.break);
-	}, [mode]);
+  useEffect(() => {
+    setFavIcon(mode === Mode.Pomodoro ? FavIcon.work : FavIcon.break);
+  }, [mode]);
 
-	useFavicon(favIcon);
-	useDocumentTitle(`${remainingTimeText} | ${mode.toLocaleUpperCase()}`);
-	useHotkeys([
-		["Space", () => handleToggleTimer()],
-		["S", () => handleStopTimer()],
-		["N", () => handleNextTimer({ isSkip: true })],
-		["F", () => handleFullScreen()],
-	]);
+  useFavicon(favIcon);
+  useDocumentTitle(`${remainingTimeText} | ${mode.toLocaleUpperCase()}`);
+  useHotkeys([
+    ["Space", () => handleToggleTimer()],
+    ["S", () => handleStopTimer()],
+    ["N", () => handleNextTimer({ isSkip: true })],
+    ["F", () => handleFullScreen()],
+  ]);
 
-	const handleFullScreen = () => setIsFullScreen(!isFullScreen);
+  const handleFullScreen = () => setIsFullScreen(!isFullScreen);
 
-	const fullScreenStyle: Sx = useMemo(
-		() =>
-			isFullScreen
-				? {
-						width: "100vw",
-						height: "100vh",
-						position: "absolute",
-						top: 0,
-						left: 0,
-						display: "grid",
-						placeItems: "center",
-				  }
-				: {},
-		[isFullScreen],
-	);
-	return (
-		<Container>
-			<Box
-				sx={(theme) => ({
-					...fullScreenStyle,
-					minWidth: "30vw",
-					background: mode === Mode.Pomodoro ? theme.colors.red[8] : theme.colors.green[8],
-					padding: theme.spacing.md,
-					borderRadius: theme.spacing.md,
-					transition: "all .7s ease",
-				})}
-			>
-				<Center sx={{ flexDirection: "column" }}>
-					<Group>
-						<TimerMode />
-						<Button size="xs" color={mode === Mode.Pomodoro ? "red.8" : "green.8"} onClick={handleFullScreen}>
-							<RiFullscreenFill />
-						</Button>
-					</Group>
-					<TimerText />
-					<TimerControllers
-						mode={mode}
-						handleNextTimer={handleNextTimer}
-						handleStopTimer={handleStopTimer}
-						handleToggleTimer={handleToggleTimer}
-						isPlaying={isRunning}
-					/>
-					<TimerInfo />
-				</Center>
-			</Box>
-		</Container>
-	);
+  const fullScreenStyle: Sx = useMemo(
+    () =>
+      isFullScreen
+        ? {
+            width: "100vw",
+            height: "100vh",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            display: "grid",
+            placeItems: "center",
+          }
+        : {},
+    [isFullScreen]
+  );
+  return (
+    <Container>
+      <Box
+        sx={(theme) => ({
+          ...fullScreenStyle,
+          minWidth: "30vw",
+          background:
+            mode === Mode.Pomodoro
+              ? theme.colors.red[8]
+              : theme.colors.green[8],
+          padding: theme.spacing.md,
+          borderRadius: theme.spacing.md,
+          transition: "all .7s ease",
+        })}
+      >
+        <Center sx={{ flexDirection: "column" }}>
+          <Group>
+            <TimerMode />
+            <Button
+              size="xs"
+              title="Full Screen <F>"
+              color={mode === Mode.Pomodoro ? "red.8" : "green.8"}
+              onClick={handleFullScreen}
+            >
+              <RiFullscreenFill />
+            </Button>
+          </Group>
+          <TimerText />
+          <TimerControllers
+            mode={mode}
+            handleNextTimer={handleNextTimer}
+            handleStopTimer={handleStopTimer}
+            handleToggleTimer={handleToggleTimer}
+            isPlaying={isRunning}
+          />
+          <TimerInfo />
+        </Center>
+      </Box>
+    </Container>
+  );
 };
 
 export default memo(Timer);
