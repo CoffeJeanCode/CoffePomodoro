0999e5346a5970daf2aa171a54c99b2fbe443ea9
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2023-08-19 18:12:47 +0000

    hotfix: back to reset info individually

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,38 +10,40 @@ import { useLocalStorage } from "@mantine/hooks";
 import { useLayoutEffect } from "react";
 
 const Home = () => {
-	const { date, resetInfo } = useInfoState();
-	const resetStats = useStatsState((stats) => stats.resetStats);
-	const [startOfWeekDate, setStartOfWeekDate] = useLocalStorage({
-		key: "startOfWeek",
-		defaultValue: new Date(),
-	});
+  const { date, resetInfo } = useInfoState();
+  const resetStats = useStatsState((stats) => stats.resetStats);
+  const [startOfWeekDate, setStartOfWeekDate] = useLocalStorage({
+    key: "startOfWeek",
+    defaultValue: new Date(),
+  });
 
-	useLayoutEffect(() => {
-		const today = new Date(date.raw);
-		const startOfWeekValue: Date = startOfWeek(today, 1);
-		if (isToday(startOfWeekValue) || !isToday(startOfWeekDate)) {
-			setStartOfWeekDate(startOfWeekValue);
-			resetInfo();
-			resetStats();
-		}
-	}, []);
+  useLayoutEffect(() => {
+    const today = new Date(date.raw);
+    const startOfWeekValue: Date = startOfWeek(today, 1);
 
-	return (
-		<Container>
-			<Center sx={{ flexDirection: "column" }}>
-				<Title order={1} color="white" my={10}>
-					CoffePomodoro
-				</Title>
-				<Group mb={20}>
-					<Settings />
-					<Stats />
-					<Helps />
-				</Group>
-				<Timer />
-			</Center>
-		</Container>
-	);
+    if (!isToday(today)) resetInfo();
+    if (isToday(startOfWeekValue) || !isToday(startOfWeekDate)) {
+      setStartOfWeekDate(startOfWeekValue);
+      resetInfo();
+      resetStats();
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Center sx={{ flexDirection: "column" }}>
+        <Title order={1} color="white" my={10}>
+          CoffePomodoro
+        </Title>
+        <Group mb={20}>
+          <Settings />
+          <Stats />
+          <Helps />
+        </Group>
+        <Timer />
+      </Center>
+    </Container>
+  );
 };
 
 export default Home;
