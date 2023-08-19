5666a7d820e0e4db8d6a07e96e38265eb991840f
Jean Pierre Ortiz Murcia <45444014+CoffeJeanCode@users.noreply.github.com>
2023-08-19 23:31:50 +0000

    fix: break relation of curren timer and schema creation

--- src/components/Timer/useTimer.ts
+++ src/components/Timer/useTimer.ts
@@ -5,121 +5,147 @@ import { useStatsState } from "@/stores/states/stats";
 import { showNotification } from "@/utils/notification.utils";
 import { useEffect, useMemo, useRef } from "react";
 import useSound from "use-sound";
-import { getToday, millisecondsToSeconds, secondsToMilliseconds } from "../../utils/time.util";
+import {
+  getToday,
+  millisecondsToSeconds,
+  secondsToMilliseconds,
+} from "../../utils/time.util";
 
 const useTimer = () => {
-	const { config } = useConfigState();
-	const { schemas, currentSchemaId, findCurrentSchema } = useSchemasState();
-	const { date, favIcon, mode, pomodoros, sessions, setMode, setPomodoros, setSessions } = useInfoState();
-	const { remainingTime, remainingTimeText, isRunning, setIsRunning, setFinishTime, setRemainingTime } =
-		useTimerState();
-	const updateDailyStats = useStatsState((stats) => stats.updateDailyStats);
+  const { config } = useConfigState();
+  const { schemas, currentSchemaId, findCurrentSchema } = useSchemasState();
+  const {
+    date,
+    favIcon,
+    mode,
+    pomodoros,
+    sessions,
+    setMode,
+    setPomodoros,
+    setSessions,
+  } = useInfoState();
+  const {
+    remainingTime,
+    remainingTimeText,
+    isRunning,
+    setIsRunning,
+    setFinishTime,
+    setRemainingTime,
+  } = useTimerState();
+  const updateDailyStats = useStatsState((stats) => stats.updateDailyStats);
 
-	const { timers, notification, behaviur } = currentSchemaId === "" ? config : findCurrentSchema() ?? config;
-	const [playNotification] = useSound(notification.alarm.url, {
-		volume: notification.volume,
-	});
+  const { timers, notification, behaviur } =
+    currentSchemaId === "" ? config : findCurrentSchema() ?? config;
+  const [playNotification] = useSound(notification.alarm.url, {
+    volume: notification.volume,
+  });
 
-	// rome-ignore lint: romelint/suspicious/noExplicitAny
-	const intervalRef = useRef<any>(null);
+  // rome-ignore lint: romelint/suspicious/noExplicitAny
+  const intervalRef = useRef<any>(null);
 
-	const nextRemainingTime = timers[mode];
-	const calculatedToLongBreak = useMemo(() => behaviur.pomodorosToLongBreak * 2 - 1, [behaviur]);
+  const nextRemainingTime = timers[mode];
+  const calculatedToLongBreak = useMemo(
+    () => behaviur.pomodorosToLongBreak * 2 - 1,
+    [behaviur]
+  );
 
-	useEffect(() => {
-		const then = Date.now() + secondsToMilliseconds(remainingTime);
-		setFinishTime(then);
+  useEffect(() => {
+    const then = Date.now() + secondsToMilliseconds(remainingTime);
+    setFinishTime(then);
 
-		intervalRef.current = setInterval(() => {
-			if (!isRunning) clearInterval(intervalRef.current);
-			else {
-				const secondsLeft = Math.round(millisecondsToSeconds(then - Date.now()));
-				setRemainingTime(secondsLeft);
-			}
+    intervalRef.current = setInterval(() => {
+      if (!isRunning) clearInterval(intervalRef.current);
+      else {
+        const secondsLeft = Math.round(
+          millisecondsToSeconds(then - Date.now())
+        );
+        setRemainingTime(secondsLeft);
+      }
 
-			if (remainingTime <= 1) {
-				clearInterval(intervalRef.current);
-				handleEndTimer();
-				if (mode === Mode.Pomodoro) {
-					handleComplete();
-				}
-			}
-		}, 1000);
+      if (remainingTime <= 1) {
+        clearInterval(intervalRef.current);
+        handleEndTimer();
+        if (mode === Mode.Pomodoro) {
+          handleComplete();
+        }
+      }
+    }, 1000);
 
-		return () => clearInterval(intervalRef.current);
-	}, [isRunning, timers, remainingTime]);
+    return () => clearInterval(intervalRef.current);
+  }, [isRunning, timers, nextRemainingTime]);
 
-	useEffect(() => {
-		setRemainingTime(nextRemainingTime);
-	}, [mode, timers, schemas, nextRemainingTime]);
+  useEffect(() => {
+    setRemainingTime(nextRemainingTime);
+  }, [mode, timers, nextRemainingTime]);
 
-	const handleNextTimer = ({ isSkip }: { isSkip: boolean }) => {
-		if (isSkip) setMode(mode === Mode.Pomodoro ? Mode.ShortBreak : Mode.Pomodoro);
-		else switchMode();
-		setIsRunning(behaviur.canAutoPlay);
-		setPomodoros(pomodoros > calculatedToLongBreak ? 1 : pomodoros + 1);
-	};
+  const handleNextTimer = ({ isSkip }: { isSkip: boolean }) => {
+    if (isSkip)
+      setMode(mode === Mode.Pomodoro ? Mode.ShortBreak : Mode.Pomodoro);
+    else switchMode();
+    setIsRunning(behaviur.canAutoPlay);
+    setPomodoros(pomodoros > calculatedToLongBreak ? 1 : pomodoros + 1);
+  };
 
-	const switchMode = () =>
-		setMode(
-			pomodoros % calculatedToLongBreak === 0
-				? Mode.LongBreak
-				: mode === Mode.Pomodoro
-				? Mode.ShortBreak
-				: Mode.Pomodoro,
-		);
+  const switchMode = () =>
+    setMode(
+      pomodoros % calculatedToLongBreak === 0
+        ? Mode.LongBreak
+        : mode === Mode.Pomodoro
+        ? Mode.ShortBreak
+        : Mode.Pomodoro
+    );
 
-	const handleToggleTimer = () => setIsRunning(!isRunning);
+  const handleToggleTimer = () => setIsRunning(!isRunning);
 
-	const handleStopTimer = () => {
-		if (mode !== Mode.Pomodoro) return;
-		setRemainingTime(nextRemainingTime);
-		setIsRunning(behaviur.canAutoPlay);
-	};
+  const handleStopTimer = () => {
+    if (mode !== Mode.Pomodoro) return;
+    setRemainingTime(nextRemainingTime);
+    setIsRunning(behaviur.canAutoPlay);
+  };
 
-	const handleEndTimer = () => {
-		sendNotification();
-		handleNextTimer({ isSkip: false });
-	};
+  const handleEndTimer = () => {
+    sendNotification();
+    handleNextTimer({ isSkip: false });
+  };
 
-	const handleComplete = () => {
-		setSessions(sessions + 1);
-		updateDailyStats(getToday(date.raw), {
-			sessions,
-			time: timers[Mode.Pomodoro],
-		});
-	};
+  const handleComplete = () => {
+    setSessions(sessions + 1);
+    updateDailyStats(getToday(date.raw), {
+      sessions,
+      time: timers[Mode.Pomodoro],
+    });
+  };
 
-	const sendNotification = () => {
-		playNotification();
+  const sendNotification = () => {
+    playNotification();
 
-		if (!notification.desktopNofitication) return;
+    if (!notification.desktopNofitication) return;
 
-		const notificationBody =
-			mode === Mode.Pomodoro
-				? "Well done! Work mode complete. Take a break and recharge!"
-				: mode === Mode.ShortBreak
-				? "Break's over! Time to get back in action!"
-				: mode === Mode.LongBreak
-				? "You rocked the break! Let's get back to work."
-				: "Timer finish";
-		showNotification("Timer has finished", notification.desktopNofitication, {
-			lang: "en",
-			body: notificationBody,
-			icon: favIcon,
-			vibrate: [100, 200, 300],
-		});
-	};
+    const notificationBody =
+      mode === Mode.Pomodoro
+        ? "Well done! Work mode complete. Take a break and recharge!"
+        : mode === Mode.ShortBreak
+        ? "Break's over! Time to get back in action!"
+        : mode === Mode.LongBreak
+        ? "You rocked the break! Let's get back to work."
+        : "Timer finish";
+    showNotification("Timer has finished", notification.desktopNofitication, {
+      lang: "en",
+      body: notificationBody,
+      icon: favIcon,
+      vibrate: [100, 200, 300],
+    });
+  };
 
-	return {
-		handleNextTimer,
-		handleStopTimer,
-		switchMode,
-		handleToggleTimer,
-		isRunning,
-		remainingTime,
-		remainingTimeText,
-	};
+  return {
+    handleNextTimer,
+    handleStopTimer,
+    switchMode,
+    handleToggleTimer,
+    isRunning,
+    remainingTime,
+    remainingTimeText,
+  };
 };
 
 export default useTimer;
