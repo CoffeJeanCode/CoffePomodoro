5647cfa99ea091d2293b5dbf7e3b74457ee1ace2
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2023-09-30 01:39:47 +0000

    hotfix: reset init state of stats

--- src/stores/states/stats.ts
+++ src/stores/states/stats.ts
@@ -10,22 +10,22 @@ interface StatsState extends Stats {
 
 const initialState: Stats = {
   stats: {
-    [String(Weekday.Monday)]: { sessions: 1, time: 1 },
-    [String(Weekday.Tuesday)]: { sessions: 3, time: 2 },
-    [String(Weekday.Wednesday)]: { sessions: 2, time: 3 },
-    [String(Weekday.Thursday)]: { sessions: 5, time: 4 },
-    [String(Weekday.Friday)]: { sessions: 5, time: 5 },
-    [String(Weekday.Saturday)]: { sessions: 1, time: 6 },
-    [String(Weekday.Sunday)]: { sessions: 0, time: 1 },
+    [String(Weekday.Monday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Tuesday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Wednesday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Thursday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Friday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Saturday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Sunday)]: { sessions: 0, time: 0 },
   },
   prevWeekStats: {
-    [String(Weekday.Monday)]: { sessions: 4, time: 1 },
-    [String(Weekday.Tuesday)]: { sessions: 1, time: 2 },
-    [String(Weekday.Wednesday)]: { sessions: 4, time: 3 },
-    [String(Weekday.Thursday)]: { sessions: 6, time: 4 },
-    [String(Weekday.Friday)]: { sessions: 1, time: 5 },
-    [String(Weekday.Saturday)]: { sessions: 5, time: 6 },
-    [String(Weekday.Sunday)]: { sessions: 1, time: 1 },
+    [String(Weekday.Monday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Tuesday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Wednesday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Thursday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Friday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Saturday)]: { sessions: 0, time: 0 },
+    [String(Weekday.Sunday)]: { sessions: 0, time: 0 },
   },
 };
 
