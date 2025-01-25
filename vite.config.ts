20907e66b4f4f6db10eb8c90effee464d0339f2b
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2025-01-25 01:46:39 +0000

    dev: add watch polling for wsl

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,10 @@ export default defineConfig({
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
