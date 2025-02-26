e5489df99230ce898191e8736f25bb903e596edd
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2025-02-26 04:29:44 +0000

    feat: add default schemas

--- src/stores/states/schema.ts
+++ src/stores/states/schema.ts
@@ -1,8 +1,11 @@
+import { Mode } from "@/models";
 import type { Schemas, TimerSchema } from "@/models/schemas";
 import { createId } from "@/utils/extra.utils";
+import { minutesToSeconds } from "@/utils/time.util";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { storeVersion } from "../config";
+import { ALARMS } from "../constants";
 
 export interface SchemasState extends Schemas {
 	addSchema: (schema: Omit<TimerSchema, "id">) => void;
@@ -13,14 +16,70 @@ export interface SchemasState extends Schemas {
 	updateCurrentSchema: (updatedSchema: TimerSchema) => void;
 }
 
+const defaultSchemas: TimerSchema[] = [
+	{
+		id: createId(),
+		title: "Work Session",
+		timers: {
+			[Mode.Pomodoro]: minutesToSeconds(25),
+			[Mode.ShortBreak]: minutesToSeconds(5),
+			[Mode.LongBreak]: minutesToSeconds(15),
+		},
+		notification: {
+			alarm: ALARMS.Rise,
+			desktopNotification: true,
+			volume: 70,
+		},
+		behaviur: {
+			canAutoPlay: true,
+			pomodorosToLongBreak: 4,
+		},
+	},
+	{
+		id: createId(),
+		title: "Reading",
+		timers: {
+			[Mode.Pomodoro]: minutesToSeconds(30),
+			[Mode.ShortBreak]: minutesToSeconds(7),
+			[Mode.LongBreak]: minutesToSeconds(20),
+		},
+		notification: {
+			alarm: ALARMS.Micellaneus,
+			desktopNotification: true,
+			volume: 50,
+		},
+		behaviur: {
+			canAutoPlay: false,
+			pomodorosToLongBreak: 3,
+		},
+	},
+	{
+		id: createId(),
+		title: "Study",
+		timers: {
+			[Mode.Pomodoro]: minutesToSeconds(45),
+			[Mode.ShortBreak]: minutesToSeconds(10),
+			[Mode.LongBreak]: minutesToSeconds(20),
+		},
+		notification: {
+			alarm: ALARMS.Shake,
+			desktopNotification: false,
+			volume: 30,
+		},
+		behaviur: {
+			canAutoPlay: true,
+			pomodorosToLongBreak: 5,
+		},
+	},
+];
 export const useSchemasState = create<SchemasState>()(
 	persist(
 		(set, get) => ({
-			schemas: [],
+			schemas: defaultSchemas,
 			currentSchemaId: "",
 			addSchema: (schema) => {
 				set(() => ({
-					schemas: [...get().schemas, { ...schema, id: createId() }],
+					schemas: [{ ...schema, id: createId() }, ...get().schemas],
 				}));
 			},
 			updateSchema: (id, updatedSchema) => {
